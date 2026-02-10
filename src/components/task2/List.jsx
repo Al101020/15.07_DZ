6a2030d925b2354ff6a48d20b3d43545994a5bda
Al101020 <alex.vl.m@yandex.ru>
2026-02-10 21:59:03 +1000

    commit-005

--- src/components/task2/List.jsx
+++ src/components/task2/List.jsx
@@ -2,9 +2,39 @@ import Video from './Video';
 import Article from './Article';
 import { v4 as uuidv4 } from 'uuid';
 
+import New from './New';
+import Popular from './Popular';
+
+// const NewVideo = New(<Video {...item} key={uuidv4()} />);
+// console.log(NewVideo);
+const NewArticle = New(Article);
+
+const PopularVideo = Popular(Video);
+const PopularArticle = Popular(Article);
+
 export default function List(props) {
   return props.list.map(item => {
-    console.log(item.views);
+    // console.log(item);
+    if (item.views < 100) {
+      const NewVideo = New(<Video {...item} key={uuidv4()} />);
+      console.log(NewVideo);
+      switch (item.type) {
+        case 'video':
+          return (
+            <Video {...item} key={uuidv4()} />
+          );
+  
+        case 'article':
+          return (
+            <Article {...item} key={uuidv4()} />
+          );
+      }
+
+      // console.log(item.views + ' < 100');
+    } else if (item.views > 1000) {
+      // console.log(item.views + ' > 1000');
+    }
+
     switch (item.type) {
       case 'video':
         return (
